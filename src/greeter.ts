4e8421b9598c26b4056506edef13085d6dcffc3b
mr838wind <mr838wind@gmail.com>
2017-09-07 14:19:15 +0900

    typescript example

--- src/greeter.ts
+++ src/greeter.ts
@@ -1,6 +1,18 @@
-function greeter(person: string) {
-    return "Hello, " + person;
+interface Person {
+    name: string;
 }
 
+class Men implements Person {
+    public name:string;
+    constructor(name:string) {
+      this.name = 'Mr ' + name;
+    }
+}
+
+
+function greeter(person: string) {
+    let user = new Men("Jack");
+    return "Hello, " + user.name;
+}
 
 export default greeter;
